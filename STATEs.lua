5a979fe56315b5df35e03604144790a1440dd872
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-07 16:07:32 +0500

    Create STATEs.lua

---
+++ STATEs.lua
@@ -0,0 +1,4 @@
+local state1=not;local state2=if;local state3=then;local state3=while;local state4=true;local state5=false;local state6="ON";local state7="OFF";local state8="Disable";local state9="Enable"
+
+  -- Example statement
+  if state1 then print(state6) end 
