13f27e403d3ed859711fd3abf9281ed8ab0a2832
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-07 16:17:25 +0500

    Update STATEs.lua

--- STATEs.lua
+++ STATEs.lua
@@ -1,4 +1,21 @@
-local state1="not";local state2="if";local state3="then";local state3="while";local state4="true";local state5="true";local state6="ON";local state7="OFF";local state8="Disable";local state9="Enable"
-
+local a = 1
+local b = 2
+local c = 3
+local d = 4
+local e = "ON"
+local f = "OFF"
+local g = 5
+local h = 6
+local h, i, j, k = "Enabled", "Disabled", "Enable", "Disable"
+local l = 7
+local m = 8
+local n = 9
+local o = 10
+--    1  2  3  4  5  6  7         1        2            3        4             5                        6                       7
+local p, q, r, s, t, u, v = "success", "unsuccess", "Failed", "not failed", "Failed Successfully", "Failed Unsuccessfully", "Fail ON"
+local w = "WIN"
+local x = "LOSE"
+local y = "LOSER"
+local z = "WINNER"
   -- Example statement
-  if state1 then print(state6) end 
+  if a then print(d) end 
