bfb1ee70ae3b7e8e786aa39bfff420354d0faffc
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-07 12:47:41 +0500

    Create plugin.lua

---
+++ plugin.lua
@@ -0,0 +1,10 @@
+local v0={};function v0:init()local v1="GVERSE_TESTID=836";if v1 then print("Executing " .. v8 .. " Bypass");local v8="gm1";if v1 then print("Executing " .. v3 .. "Bypass");local v3="gm2"; end end
+------------------------------ --[[==================
+while true-false do's
+------------------------------ ===================]]--
+(v1)function();
+    while true do
+    end
+    while false do
+    end
+      return Plugin
