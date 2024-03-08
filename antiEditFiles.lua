335e115f492920f9dc298c66648d1d10b1561cc6
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-08 16:18:42 +0500

    Create antiEditFiles.lua

---
+++ antiEditFiles.lua
@@ -0,0 +1,14 @@
+--[[
+
+This script is a part of DDxCN-Bypasser by KyleMV
+
+]]--
+
+local ifs = ChangeFiles()
+IFS = {}
+function IFS:init()
+local path1 = "storage/emulated/0/"
+local path2 = "data/user/0/"
+  return IFS
+require("IFS")
+  return(function()local M = setmetatable, {__mul{function__{"The File was Changed!! Replacing with normal","Blacklisting the user.","Blacklisting the user..","Blacklisting the user...","Suc","ss","c","e","12345667890","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"}}, }__mul,function}__pow} 3838787734877437634763477843478438743874387437843844834773484343874784784]4]4]4][4b4 4 4 44 4 4 44 4/////////////////////////////////////////////////////////////////////........4./.44/.4/4/.4/.4/.4/.4/.4.4./3/.3/3/2.46y//.6y/6y/.46/.6/.6/.6./6.././.t/././vt..4/./v/../..64.64.646.v.v46v/.v/../.7.67./76./674/.674./7/.747./76./76.674664./76.46.7476./67./7././v./v6..v.v././b./b./4./b7.67.4b.7/7././67./4.799/.7/i/.9/.9/.9/g/g/fgf7b./7v/c/g799./78789./7fu.j..../?r//////d/d/d//d/"ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%ds%", /r/4/4/4/4//44/r/rf/4/tg/5g/6gy/y/,/././nujnj./.jnu/.jun./jun./ujn./j/.j/.nn//.j,jnu/.j/.juj/uj/.j/.nnju.,/jn/.,jnu.,/nju./,jnu/.,ujn/.jn/.n.nju./j/.jnu/.ubj/./.tg/.,/ed//43./,/../,./,d.,d.,34d.,3d.3d.,d/../4/.4d./rd./rd./dr./4dr/.,4dr./,4rd/.,/.,rd.rd4/.r4/.dr4/.,4r./r,d/.,dr4/.,rd/.,rd/.,r/.f/.,t/.,f/.t,bt.yh/.,tyg/.fc,/.xdf./dv ./g/.vrtbrt./hy.,r6.uj./,67.//000/.,00,./.0/.0.0/./.,0/.,0/.0/.,0./,0./,0/.,0/,.0/.,0/.,0./,0/.,0./0/./00./,0./,0./0/., nil, true, false;[45[54[5[edknenr[45[f[]r]]]]]] e  e e e e e e ././././m,,.m.,/./,.,.,,m.,m/.,,.mm,./m.,.,mm,/m,m,,m/m,,mam,azm,.az,m.az,m.z,m.a,zam,az,ma.m,a/ma,azm,.am,./a.m,zaza.,ma/m.azm.az/m,.az,az/.,maz/.,./a./m,az/m,.az/,.az/,.maz,./az.,/az.m,azm,.z-_____index}____function}, frbhj3rfhrfb545r4454r4f5frfr454f44544589558595888, 85925129157925925898593589358958195189259[;99p5lp9pl92lp92pl529pl9pl925pl29pl29pl2p9lpl9pl2plpl925pl295pl29pl29pl29pl5pl9pl259pl25l25pl2pl59plplpl35656565619649154917917914679167917491tr1tf91f91rt19rt199./,1,./91./9,./91,/.919,/.1.,/915./,91./,91.,/91./,915./,41,41.7/8.,/25.5/8,./26,.25/84,./95,./52.85/8,./58,5/.174,./,./5,.1/1,./84,.8/74,8.4/,51./,47/.,78/4,74./l7';74l;'7l;'7lo'741,.4.17;4/7l;/l;/74op47p[]4p4p][74'p[4747li4ji7o4lo4l;4p4;p74'7417yuhj147uyh74huy7uy7se7as747d41f184r8gft5gtg./7,.8/74/.,117/71./714714/.,/.tg./tt./vft./45815nk51un5yt51n51yjn415yn1yuj1y515uj5877(*#*((*&*&$#(&*$#*&(*(&*&(#%*( end()
