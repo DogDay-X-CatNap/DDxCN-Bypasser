ac4041588694e47552b1980796bb6f9c60754678
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-13 13:51:38 +0500

    Create setup-guide-d-op.bat

---
+++ x64/setup-guide-d-op.bat
@@ -0,0 +1,27 @@
+@echo off
+echo this can be only run on cmd exe!! use pc
+echo this cannot be run on mobile phones
+title setup-hack
+echo made by KyleMV
+title L
+title Lo
+title Loa
+title Load
+title Loadi
+title Loading.
+title Loading..
+title Loading...
+echo Building...
+title B
+title Bu
+title Bui
+title Buil
+title Build
+title Buildi
+title Buildin
+title Building.
+title Building..
+title Building...
+echo -------------
+echo Building Success!
+echo You can exit now
